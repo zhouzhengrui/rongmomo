e1975a464cc6e20107b68d358bae2cab15017753
zhouzhengrui <zhouzhengrui@gmail.com>
2016-12-24 20:35:28 +0800

    update

---
+++ README.md
@@ -0,0 +1,70 @@
+# Readme
+
+[更新记录](#update) / [所用插件](#plugin) / [相关备注](#relevant) / [已完成页面](#pages)
+
+## GitHub Pages
+
+<https://zhouzhengrui.github.io/rongmomo/>
+
+## Update
+
+### 2012-12-23
+
+- 完善商品详情页
+- 用了部分时间测试插件, 测试手机效果并调整
+- 用了大量时间对结构和元素进行调整, 主要调整header/list/button, 为了让之后的页面可以更好的复用和维护
+
+### 2012-12-22
+
+- 完成[1-首页]
+
+### 2016-12-20
+
+- 新建项目, 上传已完成页面
+
+[⬆ back to top](#readme)
+
+## Plugin
+
+- [jQuery - 1.9.1](http://jquery.com/)
+- [开关 - switchery](http://abpetkov.github.io/switchery/)
+- [滑动(tab/slide) - swiper 3.3.1](http://idangero.us/swiper/)
+- [图片延迟加载 - lazyload 1.9.3](http://www.appelsiini.net/projects/lazyload)
+- [抛物线 - fly 1.0.0](https://github.com/amibug/fly)
+- [模态窗口 - ModalWindowEffects 1.0.0](https://github.com/codrops/ModalWindowEffects)
+- [单选复选框 - iCheck 1.0.2](http://www.bootcss.com/p/icheck/)
+- [侧滑导航 - mmenu 5.7.2](http://mmenu.frebsite.nl/)
+- [星级评分 - raty 2.7.0](http://wbotelhos.com/raty)
+- [折叠面板 - jQuery Accordion 0.0.1](https://vctrfrnndz.github.io/jquery-accordion/)
+
+[⬆ back to top](#readme)
+
+## Relevant
+
+- 广告页图片 `375*667px`
+- 介绍页图片 `350*350px`
+- 首页slideshow `375*200px`
+- 首页商品图片 `345*200px`
+- 首页技师列表图片 `65*65px`
+- 商品详情页大图 `375*256px`
+- 商品详情页滑动小图 `120*120px`
+
+[⬆ back to top](#readme)
+
+## Pages
+
+- 登录 `index.html`
+- 免密登录 `1-password-unwanted.html`
+- 忘记密码 `1-password-forget.html`
+- 重置密码 `1-password-reset.html`
+- 广告 `1-advertisement.html`
+- 介绍 `1-introduce.html`
+- 定位 `1-position.html`
+- 首页-全部项目 `1-home-all.html`
+- 首页-套餐 `1-home-package.html`
+- 首页-我的技师 `1-home-technician.html`
+- 首页-我的技师-空 `1-home-technician-empty.html`
+- 商品详情 `2-goods-content.html`
+- 商品详情-评论 `2-goods-comment.html`
+
+[⬆ back to top](#readme)
---
+++ js/scripts.js
@@ -0,0 +1,227 @@
+// fix:active
+document.body.addEventListener('touchstart', function() {});
+
+// --------------------------------------------- swiper --------------------------------------------- //
+
+// introduce slide
+var titleSlide = new Swiper('#title-slide', {
+    effect: 'fade'
+});
+var introduceInfoSlide = new Swiper('#introduce-info-slide', {
+    effect: 'fade'
+});
+var introduceImageSlide = new Swiper('#introduce-image-slide', {
+    effect: 'coverflow',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    pagination: '.introduce-pagination',
+    uniqueNavElements: false,
+    lazyLoading: true,
+    watchSlidesVisibility: true,
+    coverflow: {
+        rotate: 0,
+        stretch: -6,
+        depth: 0,
+        modifier: 1,
+        slideShadows: false
+    }
+});
+introduceImageSlide.params.control = [titleSlide, introduceInfoSlide];
+introduceInfoSlide.params.control = [titleSlide, introduceImageSlide];
+
+// image slide
+var imageSlide = new Swiper('#image-slide', {
+    loop: true,
+    autoplay: 5000,
+    lazyLoading: true,
+    lazyLoadingInPrevNext: true,
+    pagination: '.swiper-pagination'
+});
+
+// tab slide
+var tabbarSlide = new Swiper('#tabbar-slide', {
+    spaceBetween: 10,
+    hashnav:true,
+    slidesPerView: 'auto'
+});
+
+// goods slide
+var goodsSlide = new Swiper('#goods-slide', {
+    effect: 'coverflow',
+    slidesPerView: 'auto',
+    lazyLoading: true,
+    watchSlidesVisibility: true,
+    pagination: '.goods-flow-pagination',
+    paginationType: 'fraction',
+    coverflow: {
+        rotate: 0,
+        stretch: -10,
+        depth: 0,
+        modifier: 1,
+        slideShadows: false
+    }
+});
+
+// switchery
+var elems = Array.prototype.slice.call(document.querySelectorAll('.switch'));
+elems.forEach(function(html) {
+    var switchery = new Switchery(html, {
+        className: 'switch',
+        color: '#53c7c6'
+    });
+});
+
+jQuery(document).ready(function($) {
+
+    // image lazyload
+    $('img.lazyload').lazyload({
+        container: $('main.main'),
+        threshold: 200,
+        effect: 'fadeIn'
+    });
+
+    // --------------------------------------------- fly --------------------------------------------- //
+
+    // 计算offset
+    var resetOffset = function() {
+        var offset = $("#cart").offset();
+        if (offset == null) {
+            return;
+        }
+        if ($(window).height() < offset.top) {
+            offset.top = $("#cart").offset().top - $(document).scrollTop();
+        }
+        return offset;
+    }
+    var offset = resetOffset();
+    // 如果窗口大小发生变化时需要重新计算
+    window.onresize = function() {
+        offset = resetOffset(offset);
+    }
+    $(".add-cart").click(function(event) {
+        //AJAX添加到购物车
+        var flag = false;
+        //AJAX添加成功后设置flag=true
+        var addCart = $(this);
+        var flyer = $('<div class="flyer"></div>');
+        flyer.fly({
+            speed: 1.2,
+            start: {
+                left: event.clientX - 25,
+                top: event.clientY - 25
+            },
+            end: {
+                left: offset.left + 12,
+                top: offset.top + 12,
+                width: 0,
+                height: 0
+            },
+            onEnd: function() {
+                var i = parseInt($("#cart-badge").html());
+                $("#cart-badge").html(i + 1);
+                $("#cart-badge").addClass('animated bounceIn').one('webkitAnimationEnd animationend', function() {
+                    $(this).removeClass('animated bounceIn');
+                });
+            }
+        });
+    });
+
+});
+
+// swiper tab
+// var swiperTab = new Swiper('#swiperTab', {
+//     autoHeight: true,
+//     onSlideChangeStart: function() {
+//         $(".swiperTab .active").removeClass('active')
+//         $(".swiperTab a").eq(swiperTab.activeIndex).addClass('active')
+//     }
+// });
+
+// jQuery plugin
+// jQuery(document).ready(function($) {
+//
+//
+//
+//     // fancy box
+//     $(".fancyBox").fancybox({
+//         padding: 0,
+//         margin: 25,
+//         minWidth: 300,
+//         maxWidth: 1000,
+//         minHeight: 0,
+//         closeBtn: false,
+//         helpers: {
+//             overlay: {
+//                 closeClick: false
+//             }
+//         }
+//     });
+//
+//     // swiper tab
+//     $(".swiperTab a").on('touchstart mousedown', function(e) {
+//         e.preventDefault()
+//         $(".swiperTab .active").removeClass('active')
+//         $(this).addClass('active')
+//         swiperTab.slideTo($(this).index())
+//     });
+//     $(".swiperTab a").click(function(e) {
+//         e.preventDefault()
+//     });
+//
+//     // selection
+//     $(".selection .group").click(function() {
+//         $(this).addClass("active").siblings().removeClass("active");
+//     });
+//
+//     // iCheck
+//     $('.iCheckInput').iCheck({
+//         checkboxClass: 'iCheck',
+//         radioClass: 'iRadio',
+//     });
+//     // 判断选中状态
+//     // $("#submit").click(function() {
+//     //
+//     //     直接获取选中radio的值
+//     //     alert($("input[name='radio-1']:checked").val());
+//     //
+//     //     循环判断是否选中
+//     //     $("input[name='radio-1']:radio").each(function() {
+//     //         if (true == $(this).is(':checked')) {
+//     //             alert($(this).val());
+//     //         }
+//     //     });
+//     //
+//     // });
+//
+//     // raty
+//     $('.raty').raty({
+//         starType: 'a',
+//         space: false
+//     });
+//     $('.ratyReadOnly').raty({
+//         starType: 'a',
+//         space: false,
+//         readOnly: true,
+//         score: function() {
+//             return $(this).attr('data-score');
+//         }
+//     });
+//
+//     // nav
+//     $("#sideNav").mmenu({
+//         "navbar": {
+//             "add": 'false',
+//             "title": '筛选',
+//         },
+//         extensions: [
+//             "pagedim-black"
+//         ],
+//         offCanvas: {
+//             "position": "right"
+//         }
+//     });
+//
+//     // accordion
+//     $('#accordion [data-accordion]').accordion();
+//
+// });
